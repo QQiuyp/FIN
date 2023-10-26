56a5659476da491458cd4238495e8a2e23df0ea4
Qiuyp <63159602+QQiuyp@users.noreply.github.com>
2023-10-26 15:47:40 +0800

    Update Inn.py

--- models/Inn.py
+++ models/Inn.py
@@ -40,7 +40,8 @@ class Noise_INN(nn.Module):
 
 class INN(nn.Module):
     def __init__(self):
-        super(INN, self).__init__()
+        super(INN, self).__init__(diff=False,length=30)
+        self.diff = diff
         self.inv1 = INN_block()
         self.inv2 = INN_block()
         self.inv3 = INN_block()
@@ -49,9 +50,19 @@ class INN(nn.Module):
         self.inv6 = INN_block()
         self.inv7 = INN_block()
         self.inv8 = INN_block()
+        if self.diff:
+            self.diff_layer_pre = nn.Linear(length, 64)
+            self.leakrelu = nn.LeakyReLU(inplace=True)
+            self.diff_layer_post = nn.Linear(64, length)
+
+        
 
     def forward(self, x, rev=False):
         if not rev:
+            if self.diff:
+                x1, x2 = x[0], x[1]
+                x2 = self.leakrelu(self.diff_layer_pre(x2))
+                x = [x1, x2]
             out = self.inv1(x)
             out = self.inv2(out)
             out = self.inv3(out)
@@ -69,4 +80,8 @@ class INN(nn.Module):
             out = self.inv3(out, rev=True)
             out = self.inv2(out, rev=True)
             out = self.inv1(out, rev=True)
+            if self.diff:
+                x1, x2 = out
+                x2 = self.diff_layer_post(x2)
+                out = [x1, x2]
         return out
